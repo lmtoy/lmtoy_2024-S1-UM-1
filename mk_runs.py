59209bbee18b9c16ba9f20c2fa0daaa1db71b74b
Peter Teuben <teuben@gmail.com>
2024-03-10 22:22:09 -0400

    add

--- mk_runs.py
+++ mk_runs.py
@@ -11,19 +11,20 @@ project="2024-S1-UM-1"
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
 on['foo'] = [ ]
-
+on["NGC4388+vicinity"] = [ 112458, 112459, 112461, 112462, 112466,
+                           112467, 112469, 112470,]
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1['foo']   = ""
+pars1["NGC4388+vicinity"] = "pix_list=-13,14,15"
 
 #        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
 pars2 = {}
-pars2['foo']   = "srdp=1 admit=0"
+pars2["NGC4388+vicinity"] = "bank=0 pix_list=-13"
 
 #        common parameters per source on subsequent runs (run1c, run2c), e.g. bank=1 for WARES
 pars3 = {}
-pars3['foo']   = "srdp=1 admit=0"
+pars3["NGC4388+vicinity"] = "bank=1 pix_list=-13,14,15"
 
 if __name__ == '__main__':    
     runs.mk_runs(project, on, pars1, pars2, pars3, sys.argv)
