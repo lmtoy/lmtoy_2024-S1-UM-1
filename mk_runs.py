53b4f55384cafeea600c4dfc50a43d87b22d6338
LMT Helpdesk <mwatstein@umass.edu>
2024-09-04 17:01:01 +0000

    commit in prep for archive upload by 09/17/24

--- mk_runs.py
+++ mk_runs.py
@@ -16,22 +16,23 @@ on["NGC4388+vicinity"] = [
 			   112708, 112709, 112713, 112714, 112718, 112719, 112723, 
                            # 112724, 
 	                   112826, 112827, 112985, 112986, 112988, 112989, 
-                           112995, 112996,
+                           -112995, 112996,
                            113129, 113130, 113132, 113133,
                            113356, 113357, 113359, 113360, 113480, 113481, 113483, 113484,
+                           116505, 116506, 117006, 117007, 117009, 117101,
                            # inherit from 2023-S1-UM-8
 			   109948, 109949, 109953, 109954, 109960, 109961, 110036, 110037,
 			 ]
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1["NGC4388+vicinity"] = "dv=250 dw=300 b_order=1"
+pars1["NGC4388+vicinity"] = "pix_list=-13  dv=250 dw=300 b_order=1 qagrade=3"
 #                         = "dv=350 dw=400 b_order=1 otf_cal=1"   # from UM-8
 
 
 #        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
 pars2 = {}
-pars2["NGC4388+vicinity"] = ""
+pars2["NGC4388+vicinity"] = "pix_list=-13 qagrade=3"
 
 # no pars3, only bank=0 was observed
 
